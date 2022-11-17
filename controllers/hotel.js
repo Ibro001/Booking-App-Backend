5a4041f70d0c57540b07cc32505b3d8ac5b5e6a5
ibro001 <ibrahimmoses56@gmail.com>
2022-11-17 20:36:59 +0100

    added the get hotels room by id controlller

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,4 +1,5 @@
-import Hotel from "../models/Hotels.js"; 
+import Hotel from "../models/Hotels.js";
+import Room from "../models/Rooms.js"; 
 
 export const createHotel = async (req,res,next) => {
     const newHotel = new Hotel(req.body);
@@ -79,3 +80,16 @@ export const countByType = async (req,res,next) => {
     }
 }
 
+export const getHotelsRooms = async (req, res, next) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        const list = await Promise.all(hotel.rooms.map(room => {
+            return Room.findById(room);
+        }))
+        res.status(200).json(list);
+    } catch (error) {
+        next(error)
+    }
+}
+ 
+ 
